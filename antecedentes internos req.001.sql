6d66ccd8252ba329616179ca84b657b4dea9f22d
Geronimo.Somma <sommagero@gsoft.com.uy>
2020-09-07 10:32:32 -0300

    algunos avances

--- antecedentes internos req.001.sql
+++ antecedentes internos req.001.sql
@@ -1,22 +1,46 @@
 
-select count(v.id_vale) as cantidad_vales_vigentes, p.id_persona
+-- Cantidad Vales Vigentes ---
+
+select count(v.id_vale) as cantidad_vales_vigentes
 from vales v  inner join lineas_credito lc on lc.id_linea_credito = v.id_linea_credito
 inner join pdv on pdv.id_pdv = lc.id_pdv
 inner join comercios c on c.id_comercio = pdv.id_comercio
 inner join personas p on p.id_persona = c.id_titular
-where v.fecha_vencimiento_vale >= DATE_FORMAT(curdate(), '%Y%m%d') and
-	p.id_persona = 812;
+where p.id_persona = 812
+and (v.fecha_vencimiento_vale >= DATE_FORMAT(curdate(), '%Y%m%d') or v.saldo_vale > 1);
 	
 
+-- Cantidad Vales Cancelados --
 
-select v.*, p.id_persona
+select count(v.id_vale) as cantidad_vales_anulados
 from vales v  inner join lineas_credito lc on lc.id_linea_credito = v.id_linea_credito
 inner join vales_anulados va on v.id_vale = va.id_vale
 inner join pdv on pdv.id_pdv = lc.id_pdv
 inner join comercios c on c.id_comercio = pdv.id_comercio
-inner join personas p on p.id_persona = c.id_titular;
+inner join personas p on p.id_persona = c.id_titular
+where p.id_persona = 812;
 
 
+select count(v.id_vale) as cantidad_vales_cancelados 
+from vales v  inner join lineas_credito lc on lc.id_linea_credito = v.id_linea_credito
+inner join pdv on pdv.id_pdv = lc.id_pdv
+inner join comercios c on c.id_comercio = pdv.id_comercio
+inner join personas p on p.id_persona = c.id_titular
+where p.id_persona = 812
+and (v.fecha_vencimiento_vale >= DATE_FORMAT(curdate(), '%Y%m%d') or v.saldo_vale < 1);
+
+
+-- Cantidad Vales --
+
+SELECT count(v.id_vale) as cantidad_vales_total
+from vales v  inner join lineas_credito lc on lc.id_linea_credito = v.id_linea_credito
+inner join pdv on pdv.id_pdv = lc.id_pdv
+inner join comercios c on c.id_comercio = pdv.id_comercio
+inner join personas p on p.id_persona = c.id_titular
+where p.id_persona = 812;
+
+
+-- Cantidad Cuotas Devengadas Vale Vigente --
 
 select COUNT(cuo.id_cuota) as cantidad_cuotas_pagas_vale_vigente
 from vales v  inner join lineas_credito lc on lc.id_linea_credito = v.id_linea_credito
